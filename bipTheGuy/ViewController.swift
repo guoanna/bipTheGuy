6a15409c19fe684e4ae41ae9c0bc6b25c898e7dc
Anna Guo <annaguo03@gmail.com>
2019-02-06 09:56:35 -0500

    4.1 Commit: - Used Stackview for 2 buttons - @IBActionOutlets created for UIImage by using Tap Gesture Recognizer

--- bipTheGuy/ViewController.swift
+++ bipTheGuy/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey you just tapped the cookie!")
+    }
+    
 }
 
