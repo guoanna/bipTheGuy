6563fbd023ae252aac8d6f4e0f8ff1d6d0af51c0
Anna Guo <annaguo03@gmail.com>
2019-02-11 16:03:24 -0500

    4.3 Commit: - Added punch sound to animation!

--- bipTheGuy/ViewController.swift
+++ bipTheGuy/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     //Mark: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,18 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.25, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
     }
     
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load.")
+        }
+    }
     
     //Mark: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
@@ -38,6 +52,7 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
     
 }
