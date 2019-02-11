fa992179164559dfc5ef44a80ee40f1f49aa6eba
Anna Guo <annaguo03@gmail.com>
2019-02-11 15:53:27 -0500

    4.2 Commit: - Animated punch for imageToPunch using UIView.animate

--- bipTheGuy/ViewController.swift
+++ bipTheGuy/ViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Mark: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //Mark: Functions
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our imageToPunch to 60 pixels
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
+    }
+    
+    
+    //Mark: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -24,7 +37,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        print("Hey you just tapped the cookie!")
+        animateImage()
     }
     
 }
